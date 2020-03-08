9d55af9df9852e9130dea5ea46f11b417e9d3a99
Ubuntu <raghunathd8@gmail.com>
2020-03-08 17:48:59 +0000

    ninth commit

--- public/test.sh
+++ public/test.sh
@@ -2,17 +2,28 @@
 
 echo "Testing..."
 
-docker stop cinemapp
-docker rm cinemapp
 
-echo "Running Cinemo Internal App"
-docker run --name cinemapp -p 8181:8080 -d cinemapp 
+echo "Running Cinemo Public App"
+docker run --name cinemapp -p 8181:80 -d cinemapp 
 
 echo "Executing GET request" 
-value=$( wget --spider -S "http://localhost:8181/api/health" 2>&1 | grep "HTTP/" | awk '{print $2}' )
-if [ $value -eq "200" ]
+value1=$( wget --spider -S "http://localhost:8181/api/health" 2>&1 | grep "HTTP/" | awk '{print $2}' )
+value2=$( wget --spider -S "http://localhost:8181/api/customers/5e5303b245d0b8f27acd05dd" 2>&1 | grep "HTTP/" | awk '{print $2}' )
+
+if [ $value1 -eq "200" ]
 then
-	echo "GET request sucessfull!"
+	echo "Test-1 = = GET request sucessfull!"
+	
+	if [ $value2 -eq "200" ]
+	then
+		echo "Test-2 = = GET request sucessfull!"
+	else
+		echo "Test-2 = = GET request FAILED !!!"
+		docker stop cinemapp
+	        docker rm cinemapp
+		exit -1
+	fi
+
 	echo "Testing Done... performing cleanup..."
 	docker stop cinemapp
 	docker rm cinemapp
