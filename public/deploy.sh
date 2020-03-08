fb9bd7f24b96970f58f04a7d8d42ec3424146f7e
Ubuntu <raghunathd8@gmail.com>
2020-03-08 20:48:49 +0000

    deploy corrections commit

--- public/deploy.sh
+++ public/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-chmod 755 build_pub.sh
 ssh -t ubuntu@10.10.1.10 'rm /home/ubuntu/cinemo/ci_cd_pipe/build_pub.sh'
 scp build_pub.sh ubuntu@10.10.1.10:/home/ubuntu/cinemo/ci_cd_pipe/
 #ssh -t ubuntu@10.10.1.10 'sh /home/ubuntu/cinemo/ci_cd_pipe/build_pub.sh'
