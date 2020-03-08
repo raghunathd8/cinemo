f05e6d049b89bd81b298fda19fe17789904e9dd9
Ubuntu <raghunathd8@gmail.com>
2020-03-08 21:02:04 +0000

    deploy filename corrections 2 commit

--- internal/deploy.sh
+++ internal/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh -t ubuntu@10.10.2.10 'rm /home/ubuntu/cinemo/ci_cd_pipe/deploy_int.sh'
+ssh -t ubuntu@10.10.2.10 'test -f /home/ubuntu/cinemo/ci_cd_pipe/deploy_int.sh && rm /home/ubuntu/cinemo/ci_cd_pipe/deploy_int.sh'
 scp deploy_int.sh ubuntu@10.10.2.10:/home/ubuntu/cinemo/ci_cd_pipe/
-#ssh -t ubuntu@10.10.1.10 'sh /home/ubuntu/cinemo/ci_cd_pipe/build_int.sh'
+#ssh -t ubuntu@10.10.2.10 'sh /home/ubuntu/cinemo/ci_cd_pipe/deploy_int.sh'
--- public/deploy.sh
+++ public/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh -t ubuntu@10.10.1.10 'rm /home/ubuntu/cinemo/ci_cd_pipe/deploy_pub.sh'
+ssh -t ubuntu@10.10.1.10 'test -f /home/ubuntu/cinemo/ci_cd_pipe/deploy_pub.sh && rm /home/ubuntu/cinemo/ci_cd_pipe/deploy_pub.sh'
 scp deploy_pub.sh ubuntu@10.10.1.10:/home/ubuntu/cinemo/ci_cd_pipe/
-#ssh -t ubuntu@10.10.1.10 'sh /home/ubuntu/cinemo/ci_cd_pipe/build_pub.sh'
+#ssh -t ubuntu@10.10.1.10 'sh /home/ubuntu/cinemo/ci_cd_pipe/deploy_pub.sh'
