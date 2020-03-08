748970db52a80498fed9ffac9dc1b4c1d50767c3
Ubuntu <raghunathd8@gmail.com>
2020-03-08 19:38:12 +0000

    transport correction commit

--- internal/transport.sh
+++ internal/transport.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-chmod 644 cinemapp_int.tar
+chmod 755 cinemapp_int.tar
 ssh -t ubuntu@10.10.2.10 'test -f /home/ubuntu/cinemo/ci_cd_pipe/cinemapp_int.tar && mv /home/ubuntu/cinemo/ci_cd_pipe/cinemapp_int.tar /home/ubuntu/cinemo/ci_cd_pipe/archive/cinemapp_int.tar_`date +'%d_%m_%Y-%H-%M'`'
 scp cinemapp_int.tar ubuntu@10.10.2.10:/home/ubuntu/cinemo/ci_cd_pipe/
--- public/transport.sh
+++ public/transport.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-chmod 644 cinemapp_pub.tar
+chmod 755 cinemapp_pub.tar
 ssh -t ubuntu@10.10.1.10 'test -f /home/ubuntu/cinemo/ci_cd_pipe/cinemapp_pub.tar && mv /home/ubuntu/cinemo/ci_cd_pipe/cinemapp_pub.tar /home/ubuntu/cinemo/ci_cd_pipe/archive/cinemapp_pub.tar_`date +'%d_%m_%Y-%H-%M'`'
 scp cinemapp_pub.tar ubuntu@10.10.1.10:/home/ubuntu/cinemo/ci_cd_pipe/
