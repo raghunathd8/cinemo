a3b2c17a7d641794c90ec35040595f1a2b9c9784
Ubuntu <raghunathd8@gmail.com>
2020-03-08 20:51:21 +0000

    deploy filename corrections commit

--- internal/deploy.sh
+++ internal/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh -t ubuntu@10.10.2.10 'rm /home/ubuntu/cinemo/ci_cd_pipe/build_int.sh'
-scp build_int.sh ubuntu@10.10.2.10:/home/ubuntu/cinemo/ci_cd_pipe/
+ssh -t ubuntu@10.10.2.10 'rm /home/ubuntu/cinemo/ci_cd_pipe/deploy_int.sh'
+scp deploy_int.sh ubuntu@10.10.2.10:/home/ubuntu/cinemo/ci_cd_pipe/
 #ssh -t ubuntu@10.10.1.10 'sh /home/ubuntu/cinemo/ci_cd_pipe/build_int.sh'
